4d013ea2d443ba94598c3c5ef4f4457c86fd21e6
mshariff81 <55815621+mshariff81@users.noreply.github.com>
2021-10-06 22:50:56 -0400

    Update with Google Sheet

---
+++ app.js
@@ -0,0 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw_Hkdq5Syrwona-SH2r3QuQhsADvQRgRg2nv_GkEXaLpnPSPI/exec'
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .then(() => {
+            //This is where you write your code
+
+
+
+
+            //Do not alter below this line
+        })
+        .catch(error => console.error('Error!', error.message))
+})
